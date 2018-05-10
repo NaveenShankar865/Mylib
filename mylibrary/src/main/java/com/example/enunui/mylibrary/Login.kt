5790de7388665d25cddb35d0ef539dcdb709070a
NaveenShankar865 <35558859+NaveenShankar865@users.noreply.github.com>
2018-05-10 09:55:36 +0530

    Update Login.kt

--- mylibrary/src/main/java/com/example/enunui/mylibrary/Login.kt
+++ mylibrary/src/main/java/com/example/enunui/mylibrary/Login.kt
@@ -3,8 +3,8 @@ package com.example.enunui.mylibrary
 import android.content.Context
 import android.widget.Toast
 
-class Login {
-    fun Log(ctx: Context, msg: String){
-        Toast.makeText(ctx,msg,Toast.LENGTH_LONG).show()
+object Login {
+    fun Log(ctx: Context,msg: String ){
+        Toast.makeText(ctx,msg,Toast .LENGTH_LONG).show()
     }
 }
